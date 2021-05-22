be3cd67682d9f942506ca400eecb5846549f8504
Vincent <github.me@5en1.com>
2021-05-22 12:04:40 +0200

    Use AccessControl only

--- contracts/ShitCoin.sol
+++ contracts/ShitCoin.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract ShitCoin is ERC20, AccessControlEnumerable, ERC20Burnable, ERC20Pausable {
+contract ShitCoin is ERC20, AccessControl, ERC20Burnable, ERC20Pausable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
@@ -19,11 +19,6 @@ contract ShitCoin is ERC20, AccessControlEnumerable, ERC20Burnable, ERC20Pausabl
         _setupRole(BURNER_ROLE, _msgSender());
         _setupRole(PAUSER_ROLE, _msgSender());
     }
-
-    function _mintMinerReward() internal {
-        _mint(block.coinbase, 1);
-    }
-
 
     /**
      * @dev Creates `amount` new tokens for `to`.
@@ -77,6 +72,10 @@ contract ShitCoin is ERC20, AccessControlEnumerable, ERC20Burnable, ERC20Pausabl
     function unpause() public virtual {
         require(hasRole(PAUSER_ROLE, _msgSender()), "ShitCoin: must have pauser role to unpause");
         _unpause();
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, 1);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
